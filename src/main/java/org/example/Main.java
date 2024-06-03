53219e076f0f2e17484bec23fac786eb865c8be3
fevzi-kayaonu <fevzikayaonu35@gmail.com>
2024-06-03 14:49:09 +0300

    testler tamamlandı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+
 public class Main {
     public static void main(String[] args) {
+
+        Contact contact = new Contact("Test", "12345678");
+        MobilePhone mobilePhone = new MobilePhone("Test");
+        mobilePhone.addNewContact(contact);
+
+        mobilePhone.removeContact(contact);
 
     }
 }
